7a84d1f83780459a77be34dc8eaa86ab5963ec50
vinsonxiao(肖宇泽) <vinsonxiao@tencent.com>
2021-07-01 16:52:26 +0800

    feat(learn): 切到lerna

--- bin/minify.js
+++ bin/minify.js
@@ -1,66 +1,70 @@
 #!/usr/bin/env node
 const { program } = require('commander');
-const Terser = require("terser");
-const fs = require("fs");
-const path = require("path");
+const Terser = require('terser');
+const fs = require('fs');
+const path = require('path');
 
 program
-	.requiredOption('-t, --target <target>', '需要压缩的目标目录');
+  .requiredOption('-p, --package <package>', '需要压缩的包名称')
+  .requiredOption('-t, --target <target>', '压缩代码输出目录');
 
 program.parse(process.argv);
 
-const target = program.target;
+const { target, package } = program;
+
+const rootDir = path.resolve(__dirname, `../packages/${package}`);
+const entryPath = path.resolve(rootDir, `${target}`);
+
+const files = getAllFiles(entryPath);
+
+minifyFiles(files);
 
 function getAllFiles(dirPath, arrayOfFiles) {
-	// console.log('dirPath', dirPath);
+  // console.log('dirPath', dirPath);
 
-	let files = fs.readdirSync(dirPath);
+  const files = fs.readdirSync(dirPath);
 
-	// console.log('files', files);
+  // console.log('files', files);
 
 
-	arrayOfFiles = arrayOfFiles || [];
+  arrayOfFiles = arrayOfFiles || [];
 
-	files.forEach(function (file) {
-		if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-			arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
-		} else {
-			arrayOfFiles.push(`${dirPath}/${file}`);
-		}
-	});
+  files.forEach((file) => {
+    if (fs.statSync(`${dirPath}/${file}`).isDirectory()) {
+      arrayOfFiles = getAllFiles(`${dirPath}/${file}`, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(`${dirPath}/${file}`);
+    }
+  });
 
-	return arrayOfFiles.filter(path => path.match(/\.js$/));
+  return arrayOfFiles.filter(path => path.match(/\.js$/));
 }
 
 async function minifyFiles(filePaths) {
-	for (let i = 0, l = filePaths.length; i < l; i++) {
-		const filePath = filePaths[i];
+  for (let i = 0, l = filePaths.length; i < l; i++) {
+    const filePath = filePaths[i];
 
-		const fileName = filePath.slice(filePath.lastIndexOf('/') + 1);
+    const fileName = filePath.slice(filePath.lastIndexOf('/') + 1);
 
-		const sourceMapPath = `${filePath}.map`;
+    const sourceMapPath = `${filePath}.map`;
 
-		// console.log('sourceMapPath', sourceMapPath);
+    // console.log('sourceMapPath', sourceMapPath);
 
-		const result = await Terser.minify(fs.readFileSync(filePath, 'utf8'), {
-			module: target === 'es',
-			sourceMap: {
-				content: fs.readFileSync(sourceMapPath, 'utf8'),
-				url: `${fileName}.map`
-			},
-		});
+    const result = await Terser.minify(fs.readFileSync(filePath, 'utf8'), {
+      module: target === 'es',
+      sourceMap: {
+        content: fs.readFileSync(sourceMapPath, 'utf8'),
+        url: `${fileName}.map`,
+      },
+    });
 
-		// console.log('result', result);
+    // console.log('result', result);
 
-		const { code, map } = result;
+    const { code, map } = result;
 
-		fs.writeFileSync(filePath, code);
-		fs.writeFileSync(sourceMapPath, map);
-	}
+    fs.writeFileSync(filePath, code);
+    fs.writeFileSync(sourceMapPath, map);
+  }
 }
 
-const files = getAllFiles(path.resolve(__dirname, `../${program.target}`));
-
-// console.log('files', files);
-minifyFiles(files);
 // minifyFiles(['/Users/xyz/Sites/tencent/qcloud/qcloud_iot/iotexplorer-appdev-jssdk/appdev-jssdk/es/utils/utillib.js'])
